2ce5120fbe6acd292dbff2c4d0d5f84077a6f3d6
German <germfema@uax.es>
2023-02-09 18:29:34 +0100

    Commit inicial en develop

---
+++ src/main/java/com/uax/accesodatos/videojuegosmmobombApi/VideojuegosMmobombApiApplication.java
@@ -0,0 +1,13 @@
+package com.uax.accesodatos.videojuegosmmobombApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideojuegosMmobombApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideojuegosMmobombApiApplication.class, args);
+	}
+
+}
