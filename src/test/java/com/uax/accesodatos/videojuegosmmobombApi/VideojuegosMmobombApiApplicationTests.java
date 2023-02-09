caf9a701e0944c9762a4123bfe1e8f0c61dc3a45
MI-NOTEBOOK-PRO\German <germfema@uax.es>
2023-02-09 18:27:12 +0100

    Commit inicial.

---
+++ src/test/java/com/uax/accesodatos/videojuegosmmobombApi/VideojuegosMmobombApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uax.accesodatos.videojuegosmmobombApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideojuegosMmobombApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
