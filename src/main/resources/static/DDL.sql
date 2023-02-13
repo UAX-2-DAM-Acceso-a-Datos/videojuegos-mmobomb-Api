ecdf1d187f6c1eb1bc4f89e6c5bb4321f1f1dc40
Sergio Laclaustra <slaclaustralopez@gmail.com>
2023-02-13 12:27:54 +0100

    DDL tablas sql

---
+++ src/main/resources/static/DDL.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE videojuegos
+
+CREATE TABLE users(
+id_user INT AUTO_INCREMENT,
+username VARCHAR(100) NOT NULL,
+password VARCHAR(250) NOT NULL,
+PRIMARY KEY(id_user)
+)ENGINE=InnoDB;
+
+CREATE TABLE favoritos(
+id_videojuego INT,
+id_user INT,
+PRIMARY KEY (id_videojuego, id_user),
+FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB;
