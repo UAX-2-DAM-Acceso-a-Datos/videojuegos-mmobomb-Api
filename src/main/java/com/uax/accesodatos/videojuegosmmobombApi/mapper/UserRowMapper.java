e45b9d36602b8185fd879124624b30cef93a7927
Sergio Laclaustra <slaclaustralopez@gmail.com>
2023-02-15 09:53:41 +0100

    Rellenar clase UserDTO y UserRowMapper

--- src/main/java/com/uax/accesodatos/videojuegosmmobombApi/mapper/UserRowMapper.java
+++ src/main/java/com/uax/accesodatos/videojuegosmmobombApi/mapper/UserRowMapper.java
@@ -1,5 +1,22 @@
 package com.uax.accesodatos.videojuegosmmobombApi.mapper;
 
-public class UserRowMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.uax.accesodatos.videojuegosmmobombApi.dto.UserDTO;
+
+
+public class UserRowMapper implements RowMapper<UserDTO>{
+	@Override
+	public UserDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		UserDTO user = new UserDTO();
+		
+		user.setId_user(rs.getInt(1));
+		user.setUsername(rs.getString(2));
+		user.setPassword(rs.getString(2));
+		
+		return user;
+	}
 }
