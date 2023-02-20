8825898f5ac0cb3603167c19d14f3f1a61fe4258
DESKTOP-FIS6MVC\edu <iaedu35@gmail.com>
2023-02-20 11:32:22 +0100

    inicio la parte de busqueda

--- src/main/java/com/uax/accesodatos/videojuegosmmobombApi/controller/BusquedaController.java
+++ src/main/java/com/uax/accesodatos/videojuegosmmobombApi/controller/BusquedaController.java
@@ -1,5 +1,33 @@
 package com.uax.accesodatos.videojuegosmmobombApi.controller;
 
-public class BusquedaController {
+import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.uax.accesodatos.videojuegosmmobombApi.dto.BusquedaParamsDTO;
+import com.uax.accesodatos.videojuegosmmobombApi.dto.VideojuegosDTO;
+import com.uax.accesodatos.videojuegosmmobombApi.services.VideojuegosService;
+
+@Controller
+public class BusquedaController {
+	@GetMapping("/filter")
+	public String init(Model model) {
+		BusquedaParamsDTO searchArgs = new BusquedaParamsDTO();
+		model.addAttribute("searchArgs", searchArgs);
+		return "busquedaForm";
+	}
+
+	@PostMapping("/filter")
+	public String filterVideojuegos(@ModelAttribute("searchArgs") BusquedaParamsDTO searchArgs) {
+		ModelAndView view = new ModelAndView("index");
+		ArrayList<VideojuegosDTO> videojuegos = VideojuegosService.getListJuegosFiltered(searchArgs.getCategory(),
+				searchArgs.getPlatform());
+		view.addObject("juegos", videojuegos);
+		return "index";
+	}
 }
---
+++ src/main/java/com/uax/accesodatos/videojuegosmmobombApi/dto/BusquedaParamsDTO.java
@@ -0,0 +1,23 @@
+package com.uax.accesodatos.videojuegosmmobombApi.dto;
+
+public class BusquedaParamsDTO {
+	private String category;
+	private String platform;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getPlatform() {
+		return platform;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setPlatform(String platform) {
+		this.platform = platform;
+	}
+
+}
